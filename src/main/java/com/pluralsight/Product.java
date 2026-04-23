4747a5c3a173058b82de9aa733cb26270d5f39b5
Alexis Barrett <happynessb09@gmail.com>
2026-04-22 22:20:43 -0400

    Adding in some iteam to loop throught

--- src/main/java/com/pluralsight/Product.java
+++ src/main/java/com/pluralsight/Product.java
@@ -3,9 +3,9 @@ package com.pluralsight;
 public class Product {
     private int id;
     private String name;
-    private float price;
+    private double price;
 
-    public Product(int id, String name, float price) {
+    public Product(int id, String name, double price) {
         this.id = id;
         this.name = name;
         this.price = price;
@@ -27,11 +27,11 @@ public class Product {
         this.name = name;
     }
 
-    public float getPrice() {
+    public double getPrice() {
         return price;
     }
 
-    public void setPrice(float price) {
+    public void setPrice(double price) {
         this.price = price;
     }
 }
--- src/main/java/com/pluralsight/StoreApp.java
+++ src/main/java/com/pluralsight/StoreApp.java
@@ -6,10 +6,15 @@ import java.util.Scanner;
 public class StoreApp {
     public static HashMap<Integer, Product> inventory = new HashMap<Integer, Product>();
     public static void main(String[] args){
+
+        //loading in the method
+        loadInventory();
+
         Scanner scanner = new Scanner(System.in);
         System.out.print("What item # are you interested in? ");
         int id = scanner.nextInt();
 
+        //searches through the products
         Product matchedProduct = inventory.get(id);
         if (matchedProduct == null){
             System.out.println("We don't carry that product");
@@ -18,6 +23,19 @@ public class StoreApp {
         System.out.printf("We carry %s and the price is $%.2f", matchedProduct.getName(), matchedProduct.getPrice());
     }
 
-    private HashMap<Integer, Product> loadInventory = new HashMap<Integer, Product>();
-    
+    private static HashMap<Integer, Product>loadInventory (){
+        HashMap<Integer, Product>inventory = new HashMap<>();
+        inventory.put(6543, new Product(6543,"Bananas",2.49));
+        inventory.put(7654, new Product(7654, "Apples", 3.19));
+        inventory.put(8765, new Product(8765, "Oranges", 4.29));
+        inventory.put(9871, new Product(9871, "Milk", 2.99));
+        inventory.put(1982, new Product(1982, "Bread", 1.99));
+        inventory.put(2193, new Product(2193, "Eggs", 3.49));
+        inventory.put(3204, new Product(3204, "Cheese", 5.79));
+        inventory.put(4315, new Product(4315, "Chicken Breast", 7.99));
+        inventory.put(5426, new Product(5426, "Rice", 2.49));
+
+        return inventory;
+
+    }
 }
